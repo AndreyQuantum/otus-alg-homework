60627aade9d3c0342c993eea7d7cbf401dd5d8e7
netwengineer <zelentsov1254@gmail.com>
2024-01-16 08:49:56 +0100

    matrix -> bit position

--- src/org/azelentsov/otusHw/task06Ladja/Solution06Ladja.java
+++ src/org/azelentsov/otusHw/task06Ladja/Solution06Ladja.java
@@ -2,23 +2,13 @@ package org.azelentsov.otusHw.task06Ladja;
 
 public class Solution06Ladja {
 
-    public static long run(long mask){
-//        Высчитываем позицию ладьи ( порядковый номер бита, занятый ладьей в матрице)
-        int bitPosition = bitPosition(mask);
+    public static long run(int bitPosition){
 //        Целочисленная часть результата деления на 8 - на сколько нужно подвинуть линию горизонтали вверх
 //        Дробная часть результата деления на 8 - на сколько нужно подвинуть линию вертикали влево
-        return (255L<<((bitPosition / 8 ) * 8 ) | 72340172838076673L<<bitPosition % 8) ^ mask;
-    }
-
-    public static int bitPosition(long mask){
-        int count = 0;
-        for (;mask > 1; mask = mask >>1){
-            count++;
-        }
-        return count;
+        return (255L<<((bitPosition / 8 ) * 8 ) | 72340172838076673L<<bitPosition % 8) ^ (1L<<bitPosition);
     }
 
     public static void main(String[] args) {
-        System.out.println(Solution06Ladja.run(524288));
+        System.out.println(Solution06Ladja.run(60));
     }
 }
