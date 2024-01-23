823164a8cc1c0353c672122a17b583df2443e4bd
netwengineer <zelentsov1254@gmail.com>
2024-01-23 21:39:08 +0100

    +heap Solution12HeapSort, my attempt

--- src/org/azelentsov/otusHw/task12HeapSort/Solution12HeapSort.java
+++ src/org/azelentsov/otusHw/task12HeapSort/Solution12HeapSort.java
@@ -1,6 +1,7 @@
 package org.azelentsov.otusHw.task12HeapSort;
 
 import org.azelentsov.otusHw.common.BaseSort;
+import org.azelentsov.otusHw.task11SelectionSort.Solution11SelectionSort;
 
 public class Solution12HeapSort extends BaseSort {
 
@@ -10,6 +11,34 @@ public class Solution12HeapSort extends BaseSort {
 
     @Override
     protected void sort() {
+        for (int index = arrayToSort.length -1; index > 0; index--){
+            for (int root = (index-1)/2; root>0; root--) {
+                heapify(root, index);
+            }
+            swap(0, index);
+        }
+    }
 
+    private void heapify(int root, int end){
+        int max = root;
+        int L = (2*root)+1;
+        int R = (2*root)+2;
+        if (arrayToSort[L] > arrayToSort[root]){
+            max = L;
+        }
+        if (R<= end && arrayToSort[R] > arrayToSort[max]) {
+            max = R;
+        }
+        if (max == root){
+            return;
+        }
+        swap(root,max);
+    }
+    public static void main(String[] args) {
+        System.out.println("Element count \t elapsed time");
+        for (int N = 10; N<=1_000_000_000; N*=10){
+            var bubbleTest = new Solution12HeapSort(N);
+            bubbleTest.run();
+        }
     }
 }
