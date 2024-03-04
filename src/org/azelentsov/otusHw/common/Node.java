2f393c4d6b252c4db8560cca55d4ec04b8a50b52
netwengineer <zelentsov1254@gmail.com>
2024-03-04 21:56:52 +0100

    node -> common

--- src/org/azelentsov/otusHw/common/Node.java
+++ src/org/azelentsov/otusHw/common/Node.java
@@ -1,8 +1,8 @@
 package org.azelentsov.otusHw.common;
 
 public class Node {
-        Node left, right;
-        int value;
+        public Node left, right;
+        public int value;
 
         public Node(int value) {
             this.value = value;
