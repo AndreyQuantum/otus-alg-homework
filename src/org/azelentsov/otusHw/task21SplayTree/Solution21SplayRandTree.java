cddda1061f28a4c3d06b0d9d459a580c02b21034
AndreyQuantum <zelentsov1254@gmail.com>
2024-03-07 21:25:30 +0100

    fix splayRandTree

--- src/org/azelentsov/otusHw/task21SplayTree/Solution21SplayRandTree.java
+++ src/org/azelentsov/otusHw/task21SplayTree/Solution21SplayRandTree.java
@@ -4,56 +4,94 @@ import org.azelentsov.otusHw.common.Node;
 import org.azelentsov.otusHw.task19BST.Solution19BST;
 
 import java.util.Arrays;
+import java.util.Random;
 
-//  Наследуемся от обычного бинарного дерева, так как алгоритм изменяет фактически только функцию вставки
-public class Solution21SplayRandTree extends Solution19BST {
 
+class Solution21SplayRandTree extends Solution19BST {
 
     public Solution21SplayRandTree(int nodeNumbers) {
         super(nodeNumbers);
     }
 
-    // Функция splay
+    private Node rightRotate(Node x) {
+        Node y = x.left;
+        x.left = y.right;
+        y.right = x;
+        return y;
+    }
+
+    private Node leftRotate(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+        y.left = x;
+        return y;
+    }
+
+    // Метод для выполнения операции splay для узла с указанным ключом
     private Node splay(Node root, int key) {
+        // Если корневой узел пуст или содержит ключ, возвращаем корень
         if (root == null || root.value == key)
             return root;
 
+        // Если ключ меньше, чем значение корневого узла, работаем с левым поддеревом
         if (root.value > key) {
+            // Если левый дочерний узел отсутствует, возвращаем корень
             if (root.left == null)
                 return root;
+            // Если ключ меньше значения левого дочернего узла, выполняем двойной поворот
             if (root.left.value > key) {
+                // Первый шаг splay для левого левого потомка
                 root.left.left = splay(root.left.left, key);
+                // Правый поворот для корня
                 root = rightRotate(root);
             }
+            // Если ключ больше значения левого дочернего узла, выполняем zig-zag поворот
             else if (root.left.value < key) {
+                // Splay для левого правого потомка
                 root.left.right = splay(root.left.right, key);
+                // Если левый правый потомок не пуст, выполняем левый поворот для левого потомка
                 if (root.left.right != null)
                     root.left = leftRotate(root.left);
             }
+            // Выполняем правый поворот для корня, если левый потомок не пуст, иначе возвращаем корень
             return (root.left == null) ? root : rightRotate(root);
         }
+        // Если ключ больше, чем значение корневого узла, работаем с правым поддеревом
         else {
+            // Если правый дочерний узел отсутствует, возвращаем корень
             if (root.right == null)
                 return root;
+            // Если ключ меньше значения правого дочернего узла, выполняем zig-zag поворот
             if (root.right.value > key) {
+                // Splay для правого левого потомка
                 root.right.left = splay(root.right.left, key);
+                // Если правый левый потомок не пуст, выполняем правый поворот для правого потомка
                 if (root.right.left != null)
                     root.right = rightRotate(root.right);
             }
+            // Если ключ больше значения правого дочернего узла, выполняем двойной поворот
             else if (root.right.value < key) {
+                // Splay для правого правого потомка
                 root.right.right = splay(root.right.right, key);
+                // Левый поворот для корня
                 root = leftRotate(root);
             }
+            // Выполняем левый поворот для корня, если правый потомок не пуст, иначе возвращаем корень
             return (root.right == null) ? root : leftRotate(root);
         }
     }
 
     @Override
-    public void insert(int x) {
-        tree = insert(tree, x);
+    public void insert(int x){
+//        Делаем вставку со splay только в случае, если у нас отработала формула
+        if (new Random().nextInt() % (arrayToSort.length +1) == 0){
+            tree = insert(tree, x);
+        } else {
+            tree = addNode(tree, x);
+        }
     }
 
-    // Вставка ключа
+
     private Node insert(Node root, int key) {
         if (root == null)
             return new Node(key);
@@ -77,21 +115,6 @@ public class Solution21SplayRandTree extends Solution19BST {
         return node;
     }
 
-
-
-    private Node rightRotate(Node node){
-        var leftChild = node.left;
-        node.right = leftChild.right;
-        leftChild.right = node;
-        return leftChild;
-    }
-
-    private Node leftRotate(Node node){
-        var rightChild = node.right;
-        node.right = rightChild.left;
-        rightChild.left = node;
-        return  rightChild;
-    }
 
     public static void main(String[] args) throws RuntimeException {
         System.out.println("Element count \t elapsed time");
