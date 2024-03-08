45a3cc3b7ac4df440e04d439a4c5ab868d7a3d0b
AndreyQuantum <zelentsov1254@gmail.com>
2024-03-08 23:40:30 +0100

    fix hashTable

--- src/org/azelentsov/otusHw/task22HashTable/Solution22HashTable.java
+++ src/org/azelentsov/otusHw/task22HashTable/Solution22HashTable.java
@@ -3,6 +3,7 @@ package org.azelentsov.otusHw.task22HashTable;
 import org.azelentsov.otusHw.common.BaseTask;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 // Реализация hashTable. Ключи - String, Значения - генерик V.
@@ -26,19 +27,21 @@ public class Solution22HashTable implements BaseTask {
         }
         return null;
     }
+
     public void put(String keyToPut, Object objectToPut){
         rehash();
-        for (int i = getPosition(keyToPut); i <= hashFactor; i++){
-            if (values[i] == null){
+        for (int i = getPosition(keyToPut); i < values.length; i++){
+            if (values[i] == null || values[i].getFirst() == keyToPut){
                 values[i] = new ArrayList();
                 values[i].add(keyToPut);
                 values[i].add(objectToPut);
+                return;
             }
         }
     }
 
     public Object delete(String keyToDelete){
-        for (int i = getPosition(keyToDelete); i<= values.length; i++){
+        for (int i = getPosition(keyToDelete); i< values.length; i++){
             if (values[i].getFirst() == keyToDelete){
                 Object objectToDelete = values[i].get(1);
                 values[i] = null;
@@ -55,15 +58,18 @@ public class Solution22HashTable implements BaseTask {
     }
 
     private void rehash(){
-        if ( (float) values.length / (float) hashFactor > loadFactor){
+        if ( (float) Arrays.stream(values).count() / (float) hashFactor > loadFactor){
             hashFactor *= 2;
             var resultValues = new ArrayList[hashFactor];
 //            Проходимся по каждому элементу и переоределяем его позицию в листе
             for (ArrayList element: values){
-                int newPosition = getPosition((String) element.getFirst());
-                for (int i = newPosition; i <= values.length; i++){
-                    if (resultValues[i] == null){
-                        resultValues[i] = element;
+                if (element != null){
+                    int newPosition = getPosition((String) element.getFirst());
+                    for (int i = newPosition; i <= values.length; i++){
+                        if (resultValues[i] == null){
+                            resultValues[i] = element;
+                            break;
+                        }
                     }
                 }
             }
@@ -78,6 +84,14 @@ public class Solution22HashTable implements BaseTask {
     }
     public static void main(String[] args) {
         var objectToCheck = new Solution22HashTable();
-        System.out.println(objectToCheck.run());
+        objectToCheck.put("test1", "SomeObj1" );
+        objectToCheck.put("test2", "SomeObj2" );
+        objectToCheck.put("test3", "SomeObj3" );
+        objectToCheck.put("test101", "SomeObj101" );
+        System.out.println(objectToCheck.get("test101"));
+        System.out.println(objectToCheck.delete("test101"));
+        System.out.println(objectToCheck.get("test1"));
+        System.out.println(objectToCheck.delete("test1"));
+
     }
 }
