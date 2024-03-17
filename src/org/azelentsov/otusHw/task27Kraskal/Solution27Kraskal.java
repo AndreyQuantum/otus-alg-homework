8877bb4886f1a0cefd1de92cd60dc1413de8fb51
AndreyQuantum <zelentsov1254@gmail.com>
2024-03-17 23:37:12 +0100

    + writeTwoIntsToLong kraskal

--- src/org/azelentsov/otusHw/task27Kraskal/Solution27Kraskal.java
+++ src/org/azelentsov/otusHw/task27Kraskal/Solution27Kraskal.java
@@ -4,16 +4,12 @@ import java.util.Random;
 
 public class Solution27Kraskal {
 
+
+    public static long writeTwoIntsToLong(int a, int b){
+        return ((long) a << 32) | b;
+    }
+
     public static void main(String[] args) {
-        int oneInt = new Random().nextInt(0,999);
-        int secondInt = new Random().nextInt(0,999);
-        System.out.println(oneInt);
-        System.out.println(secondInt);
-        long longToEdit;
-        longToEdit = (long) oneInt << 32;
-        longToEdit = longToEdit | secondInt;
-        System.out.print(Integer.toBinaryString(oneInt) + ' ');
-        System.out.println(Long.toBinaryString(secondInt));
-        System.out.println(Long.toBinaryString(longToEdit));
+
     }
 }
