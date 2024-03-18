904438f1298af9074f575ad7dc2ea1d04e397d5b
AndreyQuantum <zelentsov1254@gmail.com>
2024-03-18 22:16:32 +0100

    fix kraskal

--- src/org/azelentsov/otusHw/task27Kraskal/Solution27Kraskal.java
+++ src/org/azelentsov/otusHw/task27Kraskal/Solution27Kraskal.java
@@ -1,6 +1,10 @@
 package org.azelentsov.otusHw.task27Kraskal;
 
+import org.azelentsov.otusHw.task05Arrays.src.model.PriorityQueue;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Solution27Kraskal {
@@ -11,28 +15,38 @@ public class Solution27Kraskal {
 
     private long[][] resultGraph;
 
+    private ArrayList<long[]> sortedR;
+
 
     public Solution27Kraskal(long[][] graph) {
         this.graph = graph;
         dsu = new DisjointUnion(graph.length);
+        sortedR = new ArrayList<>();
         //        1. Создаем граф и переносим все его вершины
         resultGraph = new long[graph.length][graph.length];
     }
 
     public long[][] run(){
+//       2.1 добавляем все вершины графа в лист
+        for (int i = 0; i < graph.length; i++) {
+            for (int r = 0; r < graph[i].length; r++) {
+                sortedR.add(new long[]{(long)i, graph[i][r]});
+            }
+        }
+//       2.2 Сортируем ребра от каждой вершины по весу от меньшего к большему
+        for (int i = 0; sortedR.size() > 1 && i < sortedR.size() -1; i++){
 
-//       2. Сортируем ребра от каждой вершины по весу от меньшего к большему
-        for (int i = 0; i < graph.length; i++){
-            for (int r = 0; r < graph[i].length-1; r++){
 //                Делаем пока что bubble sort
 //                TODO: сделать quicksort
-                int currentEl =  readInt(graph[i][r])[1];
-                int nextEl = readInt(graph[i][r+1])[1];
+                int currentEl =  readInt(sortedR.get(i)[1])[1];
+                int nextEl =readInt(sortedR.get(i+1)[1])[1];
                 if (currentEl > nextEl){
-                    long next = graph[i][r+1];
-                    graph[i][r+1] = graph[i][r];
-                    graph[i][r] = next;
-                }
+                    long[] next = sortedR.get(i+1);
+                    sortedR.remove(i+1);
+                    sortedR.remove(i);
+                    sortedR.add(i+1, sortedR.get(i));
+                    sortedR.add(i, next);
+
             }
         }
         int rCount = 0;
@@ -45,29 +59,21 @@ public class Solution27Kraskal {
     }
 
     private void addMinR() {
-//        Находим ребро, у которой первый элемент в связи будет минимальным
-        int min = 0;
-        int minR = 0;
-        for (int i = 0; i< graph.length; i++){
-            if (graph[i].length >0 && readInt(graph[i][0])[1] < min){
-                min = readInt(graph[i][0])[1];
-                minR = i;
-            }
-        }
-//        достаем ребро из исходного графа и удаляем его
-        long rebroToInsert = graph[minR][0];
+
+//        достаем ребро из отсортированного листа и удаляем его
+        long[] rebroToInsert = sortedR.removeFirst();
+        int rToConn = (int) rebroToInsert[0];
 //      проверяем, что между нашими ребрами еще нету связи в графе
-        if (!dsu.isConnected(readInt(rebroToInsert)[0],minR)){
+        if (!dsu.isConnected(readInt(rebroToInsert[1])[0],rToConn)){
 //            Добавляем связь в граф
-            for (int i = 0; i< resultGraph[minR].length; i++){
-                if (resultGraph[minR][i] == 0){
-                    resultGraph[minR][i] = rebroToInsert;
+            for (int i = 0; i< resultGraph[rToConn].length; i++){
+                if (resultGraph[rToConn][i] == 0){
+                    resultGraph[rToConn][i] = rebroToInsert[1];
                     break;
                 }
             }
 //            добавляем связь в dsu, чтобы не соединять граф в будущем
-            dsu.union(minR, readInt(rebroToInsert)[0]);
-//            удаляем ребро из графа
+            dsu.union(rToConn, readInt(rebroToInsert[1])[0]);
         }
     }
 
